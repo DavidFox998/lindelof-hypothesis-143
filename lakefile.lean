e5335e253631b8346c17206bd0e5428554f00b53
DAVID J FOX <davidjfox998@gmail.com>
2026-07-23 18:04:50 -0700

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,7 @@
 import Lake
 open Lake DSL
 
-package lindelof-hypothesis-143 where
+package «lindelof-hypothesis-143» where
   leanOptions := #[
     ⟨`pp.unicode.fun, true⟩,
     ⟨`autoImplicit, false⟩
@@ -13,4 +13,3 @@ require mathlib from git
 @[default_target]
 lean_lib LindelofHypothesis143 where
   globs := #[.submodules `lean]
-  -- This makes `lake build` build only `lean/` folder — standalone Clay style
